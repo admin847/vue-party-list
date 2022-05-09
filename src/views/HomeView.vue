d821627ed9c3352345480b78d3abbdceee27cbba
Serhii Lysenko <admin847@gmail.com>
2022-05-09 17:11:15 +0300

    Добавил подгузку при скролле и поиск

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,7 +1,8 @@
 <template>
   <h1 class="h1">Party list</h1>
+  <input @keydown.esc="searchQuery = ''" v-model="searchQuery" type="text" placeholder="Пошук..." />
   <ul class="parties-list">
-    <li v-for="party in partyStore.parties" :key="party.id" class="party-item">
+    <li v-for="party in filteredParties" :key="party.id" class="party-item">
       <router-link :to="'/party/' + party.id">
         <img :src="party.img" alt="party-item-img" />
         <div class="party-item__title">{{ party.title }}</div>
@@ -9,12 +10,52 @@
       </router-link>
     </li>
   </ul>
+  <div ref="obs" class="observer"></div>
 </template>
 
 <script setup>
+import { ref, onMounted, computed } from 'vue';
 import { usePartyStore } from '@/stores/party';
 
 const partyStore = usePartyStore();
+
+const parties = computed(() => {
+  return partyStore.parties;
+});
+
+const searchQuery = ref('');
+
+const page = ref(1);
+const itemsPerPage = 10;
+
+const filteredParties = computed(() => {
+  let start = 0;
+  let end = itemsPerPage * page.value;
+  if (searchQuery.value === '') {
+    return parties.value.slice(start, end);
+  } else {
+    return parties.value.filter((item) => {
+      return item['title']
+        .toLowerCase()
+        .includes(searchQuery.value.toLowerCase());
+    });
+  }
+});
+
+// ===== OBSERVER =====
+const obs = ref(null);
+
+onMounted(() => {
+  const observer = new IntersectionObserver((entries) => {
+    if (
+      entries[0].isIntersecting &&
+      page.value < parties.value.length / itemsPerPage
+    ) {
+      page.value++;
+    }
+  });
+  observer.observe(obs.value);
+});
 </script>
 
 <style lang="scss" scoped>
@@ -31,5 +72,8 @@ const partyStore = usePartyStore();
 }
 img {
   max-width: 150px;
+}
+.observer {
+  height: 20px;
 }
 </style>
