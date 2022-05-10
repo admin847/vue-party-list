775a56913e54cd2743fe07f4647d06b396f4f1f7
Serhii Lysenko <admin847@gmail.com>
2022-05-10 12:24:06 +0300

    Показываем 404-ю страницу, если пати с таким id не найдено

--- src/views/PartyView.vue
+++ src/views/PartyView.vue
@@ -1,21 +1,25 @@
 <template>
-  <h1 class="h1">
-    {{ currentParty.title }}
-  </h1>
-  <div>
-    {{ currentParty.date }}
-  </div>
-  <img :src="currentParty.img" :alt="currentParty.title" />
-  <div>
-    {{ currentParty.description }}
-  </div>
-  <button @click="showHideMap">Посмотреть на карте</button>
+  <template v-if="currentParty">
+    <h1 class="h1">
+      {{ currentParty.title }}
+    </h1>
+    <div>
+      {{ currentParty.date }}
+    </div>
+    <img :src="currentParty.img" :alt="currentParty.title" />
+    <div>
+      {{ currentParty.description }}
+    </div>
+    <button @click="showHideMap">Посмотреть на карте</button>
+  </template>
+  <e404-view v-else />
 </template>
 
 <script setup>
 import { computed, onBeforeMount, onBeforeUnmount } from 'vue';
 import { useRoute } from 'vue-router';
 import { usePartyStore } from '@/stores/party';
+import E404View from './E404View.vue';
 
 const route = useRoute();
 const partyStore = usePartyStore();
@@ -25,12 +29,9 @@ const currentParty = computed(() =>
 );
 
 onBeforeMount(() => {
-  // console.log();
-  // if (!partyStore.partiesMap.hasOwnProperty(route.params.id)) {
-  //   router.push({ path: '/E404' });
-  // } else {
-  //   }
+  if (currentParty.value) {
     partyStore.setCurrentPartyLocation(currentParty.value.coordinates);
+  }
 });
 
 const showHideMap = () => {
@@ -48,6 +49,7 @@ onBeforeUnmount(() => {
 
 <style lang="scss" scoped>
 img {
-  max-width: 500px;
+  width: 100%;
+  height: auto;
 }
 </style>
