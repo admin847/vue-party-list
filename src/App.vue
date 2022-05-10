d25d0a05f6f287b97e91b6b6dd425adb80bd46ce
Serhii Lysenko <admin847@gmail.com>
2022-05-10 16:45:34 +0300

    Добавил хедер

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,6 @@
 <template>
   <div class="container">
+    <the-header />
     <router-view></router-view>
       <app-map />
     <!-- <teleport to="body">
@@ -10,6 +11,7 @@
 <script setup>
 import { onMounted } from 'vue';
 import { usePartyStore } from '@/stores/party';
+import TheHeader from './components/TheHeader.vue';
 import AppMap from '@/components/AppMap.vue';
 
 const partyStore = usePartyStore();
---
+++ src/components/TheHeader.vue
@@ -0,0 +1,29 @@
+<template>
+  <header class="header">
+    <router-link to="/">
+      <div class="logo">Parteez</div>
+    </router-link>
+  </header>
+</template>
+
+<style lang="scss" scoped>
+.header {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  padding: 50px 0;
+  border-bottom: 2px solid $color-white;
+}
+
+.logo {
+  font-size: 25px;
+  transition: color 0.35s;
+  line-height: 120%;
+  font-family: 'ST-SimpleSquare', sans-serif;
+  color: $color-white;
+  letter-spacing: 4%;
+  &:hover {
+    color: $color-accent;
+  }
+}
+</style>
